7f533976ab62a18b72a11ed9e42a280b13c8fddb
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-03-27 11:50:31 -0300

    Aula de aprendizado NextJS

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+      <div className={`
+          flex h-screen justify-center items-center
+          
+          `}>
+
+            <span className="text-4xl">Texto!!</span>
+      </div>
+  )
+}
