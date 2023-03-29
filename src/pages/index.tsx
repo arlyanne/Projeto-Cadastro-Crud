ee097df1265e6229d361eee91e53e8ce3431fc7e
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-03-29 15:02:30 -0300

    Inclusao de tabelas com tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,40 @@
 import React from "react";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+  
+  const clientes = [
+    new Cliente('Anna', 22, '1'),
+    new Cliente('Davi', 25, '2'),
+    new Cliente('Arlyanne', 28, '3'),
+    new Cliente('Amora', 33, '4'),
+    new Cliente('Chicó', 36, '5')
+    
+  ]
+
+  function clienteSelecionado(cliente : Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente : Cliente) {
+    console.log(`Excluir... ${cliente.nome}`)
+  }
+  
   return (
       <div className={`
-          flex h-screen justify-center items-center
+           flex justify-center items-center h-screen
+           bg-gradient-to-r from-blue-500 to-purple-500
+            text-white`
+      }>
+        <Layout titulo="Cadastro Simples">
+              <Tabela clientes={clientes}
+                  clienteSelecionado={clienteSelecionado}
+                  clienteExcluido={clienteExcluido}
+              />
+        </Layout> 
           
-          `}>
-
-            <span className="text-4xl">Texto!!</span>
       </div>
   )
 }
